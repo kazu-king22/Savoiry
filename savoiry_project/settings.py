d49ac7e5eb3df2f0118499c3f51964215113ec96
Kazuki Sekishita <sekishita.kazuki.0906@outlook.jp>
2025-09-02 03:10:23 +0900

    ログイン機能の為の設定追記

--- savoiry_project/settings.py
+++ savoiry_project/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = "account:login"      
+LOGIN_REDIRECT_URL = "account:index"      
+LOGOUT_REDIRECT_URL = "account:top"      
