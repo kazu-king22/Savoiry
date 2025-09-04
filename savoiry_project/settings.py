8a811d81ec38c49f26e95e63cc9f015ead3fb791
Kazuki Sekishita <sekishita.kazuki.0906@outlook.jp>
2025-09-05 02:49:05 +0900

    ログイン機能実装の為の修正

--- savoiry_project/settings.py
+++ savoiry_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +123,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'accounts.User'
 
 LOGIN_URL = "account:login"      
-LOGIN_REDIRECT_URL = "account:index"      
-LOGOUT_REDIRECT_URL = "account:top"      
+LOGIN_REDIRECT_URL = reverse_lazy('accounts:home')      
+LOGOUT_REDIRECT_URL = "account:login"      
--- savoiry_project/urls.py
+++ savoiry_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("accounts.urls")),
+    path('home', HomeView.as_view(), name='Home'), 
 ]
