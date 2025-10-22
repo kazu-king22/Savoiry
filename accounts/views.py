f68737de8c462f2dc029f2a6a9d33d417cf1b46b
Kazuki Sekishita <sekishita.kazuki.0906@outlook.jp>
2025-10-22 11:02:56 +0900

    棒グラフ２種更新

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import SignUpForm, EmailLoginForm
 from .models import User
+from django.contrib.auth.decorators import login_required
 
 
 class SignUpView(CreateView):
@@ -28,3 +29,29 @@ class HomeView(LoginRequiredMixin, TemplateView):
 
 class MyPageView(TemplateView):
     template_name = "accounts/mypage.html"
+    
+    
+@login_required
+def mypage(request):
+    visits = Visit.objects.all()
+    print("===== DEBUG =====")
+    for v in visits:
+        print(
+            f"店舗: {v.restaurant.store_name}, "
+            f"登録ユーザー: {v.restaurant.user}, "
+            f"ログイン中ユーザー: {request.user}, "
+            f"評価: {v.rating}"
+        )
+
+    top3_visits = (
+        Visit.objects
+        .filter(restaurant__user=request.user, rating__isnull=False)
+        .values("restaurant__store_name", "restaurant__genre")
+        .annotate(avg_rating=Avg("rating"))
+        .order_by("-avg_rating")[:3]
+    )
+
+    print("==== 集計結果 ====")
+    print(list(top3_visits))
+
+    return render(request, "restaurants/mypage.html", {"top3_visits": top3_visits})
