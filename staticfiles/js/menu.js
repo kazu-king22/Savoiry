b332d8d91a5b15222006e4b6c7a354ce6d447af6
Kazuki Sekishita <sekishita.kazuki.0906@outlook.jp>
2025-11-17 23:53:39 +0900

    Remove staticfiles from repository

--- staticfiles/js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll(".bottom-nav li.active img").forEach(img => {
-    const activeSrc = img.getAttribute("data-active");
-    if (activeSrc) {
-      img.setAttribute("src", activeSrc);
-    }
-  });
-});
